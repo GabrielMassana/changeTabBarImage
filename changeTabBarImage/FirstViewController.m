6efb2962c590f532583048eef295811fec5bfd4d
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-07-10 17:34:35 +0100

    blink image

--- changeTabBarImage/FirstViewController.m
+++ changeTabBarImage/FirstViewController.m
@@ -43,6 +43,28 @@
     
     // You can use this method to push the ViewController, too.
     //[tabBarController setSelectedIndex:1];
+    
+    [self blink:((SecondViewController*)[viewControllers objectAtIndex:1])];
+}
+
+
+- (void)blink: (SecondViewController*) viewControllers {
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        // Cast the ViewController you are interested and push the image when unselected
+        viewControllers.tabBarItem.image = [[UIImage imageNamed:@""] imageWithRenderingMode:UIImageRenderingModeAutomatic];
+        
+        // Cast the ViewController you are interested and push the image when selected
+        viewControllers.tabBarItem.selectedImage = [[UIImage imageNamed:@""] imageWithRenderingMode:UIImageRenderingModeAutomatic];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+            // Cast the ViewController you are interested and push the image when unselected
+            viewControllers.tabBarItem.image = [[UIImage imageNamed:@"camera"] imageWithRenderingMode:UIImageRenderingModeAutomatic];
+            
+            // Cast the ViewController you are interested and push the image when selected
+            viewControllers.tabBarItem.selectedImage = [[UIImage imageNamed:@"camera"] imageWithRenderingMode:UIImageRenderingModeAutomatic];
+        });
+    });
 }
 
 @end
