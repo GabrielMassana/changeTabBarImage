052dc14a5838626ca24ad603f5403594507bc694
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-07-10 15:25:54 +0100

    Initial Commit

---
+++ changeTabBarImage/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  changeTabBarImage
+//
+//  Created by Gabriel Massana on 10/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ changeTabBarImageTests/changeTabBarImageTests.m
@@ -0,0 +1,34 @@
+//
+//  changeTabBarImageTests.m
+//  changeTabBarImageTests
+//
+//  Created by Gabriel Massana on 10/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface changeTabBarImageTests : XCTestCase
+
+@end
+
+@implementation changeTabBarImageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
